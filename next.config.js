1db3e12b85f8e99971b640406e8f99df6a2fc312
shrixtacy <shriyanshdash12@gmail.com>
2025-11-26 13:34:09 +0530

    Fix Vercel deployment by removing static export config

--- next.config.js
+++ next.config.js
@@ -1,11 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export',
   eslint: {
     ignoreDuringBuilds: true,
   },
   images: { 
-    unoptimized: true,
     domains: ['images.pexels.com'],
     formats: ['image/webp', 'image/avif']
   },
